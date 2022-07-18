6869ec2c75e3189914f0a9daf5cd6c8036216ed3
RATSENO <onestar1014@gmail.com>
2022-07-18 12:06:08 +0900

    CSSModule 클래스 두개 이상 사용할 경우

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
             안녕하세요 저는 <span className="something">CSS Module!</span>
         </div>
     );
