c01b3fbffbfa8ed91cfffffab2c2e6eaaa9246ca
RATSENO <onestar1014@gmail.com>
2022-07-18 12:17:40 +0900

    classNames , sass와 같이 사용하기

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,10 +1,13 @@
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
+import classNames from 'classnames/bind';
 
 import React from 'react';
 
 const CSSModule = () => {
+    const cx = classNames.bind(styles);
+
     return (
-        <div className={`${styles.wrapper} ${styles.inverted}`}>
+        <div className={cx('wrapper', 'inverted')}>
             안녕하세요 저는 <span className="something">CSS Module!</span>
         </div>
     );
